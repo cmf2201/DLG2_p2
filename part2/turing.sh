4fbd8d197d9113da30cbb01acb10a445d4cc642f
Chaddyoh <ctnguyen@wpi.edu>
2024-09-11 17:57:48 -0400

    updated turing sh

--- part2/turing.sh
+++ part2/turing.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#SBATCH --mail-user=username@wpi.edu
+#SBATCH --mail-user=ctnguyen@wpi.edu
 #SBATCH --mail-type=ALL
 
 #SBATCH -J window_seg
-#SBATCH --output=/home/username/logs/window_seg%j.out
-#SBATCH --error=/home/username/logs/window_seg%j.err
+#SBATCH --output=/home/ctnguyen/logs/window_seg%j.out
+#SBATCH --error=/home/ctnguyen/logs/window_seg%j.err
 
 #SBATCH -N 1
 #SBATCH -n 8
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 #SBATCH -C H100|A100|V100
-#SBATCH -p short
-#SBATCH -t 23:00:00
+#SBATCH -p academic
+#SBATCH -t 12:00:00
 
 python3 train.py
