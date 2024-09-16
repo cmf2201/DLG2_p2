7f09eec21357809c5486064396804499edb4bfa7
cmf2201 <cmfrench808@gmail.com>
2024-09-16 17:41:35 -0400

    add gitignore update

--- test-video/test-video.py
+++ test-video/test-video.py
@@ -13,29 +13,55 @@ from torch.utils.data import Subset
 import wandb
 
 # CUSTOM
-from part2.network import *
-from part2.utils import *
+from network import *
+from utils import *
 from dataloader import *
 
 # Load the parameters
 from loadParam import *
 torch.cuda.empty_cache()
 
+home_dir = "/home/caleb/Documents/WPI/24_25/DeepLearning/HW2/Group2_p2/test-video/"
+
+
 datatype = torch.float32
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-dataset = VideoDataset("./")
+dataset = VideoDataset(os.path.join(home_dir,'images/'))
 
-model = UNET().to("cpu")
-model.load_state_dict(torch.load("trained_model8.pth", map_location=torch.device('cpu')))
+dataset = torch.utils.data.DataLoader(dataset, 1, False, num_workers=1)
 
+model = nn.DataParallel(UNET()).to(device)
+
+model.load_state_dict(torch.load(os.path.join(home_dir,"trained_model11.pth"), map_location=torch.device('cpu'),weights_only=True))
 model.eval()
+
 for batchcount, (rgb) in enumerate(dataset):
     rgb = rgb.to(device)
     
     pred = model(rgb)
-    img = Image.fromarray(pred, 'L')
-    img.save(str(batchcount + 1) + '.png')
+
+    # # Move the tensor to the CPU, detach from computation graph, and convert to NumPy
+    pred_np = pred.detach().numpy()
+
+    #  # Optional: If the tensor has extra dimensions (batch or channel), you may need to squeeze
+    # # Assuming pred is [batch_size, channels, height, width] and you want single-channel grayscale
+    pred_np = np.squeeze(pred_np)  # Remove dimensions of size 1
+    pred_np = np.squeeze(pred_np)  # Remove dimensions of size 1
+
+    pred_np = np.clip(pred_np, 0, 1)
+
+    # # Normalize the prediction to the range [0, 255] for image saving
+    pred_np = (pred_np * 255).astype(np.uint8)
+
+    # combined_image_np = CombineVideoImages(, pred)
+
+    # img = Image.fromarray(combined_image_np, 'RGB')
+    # img.save(os.path.join(home_dir,'Outputs/Combined/') + str(batchcount + 1) + '.png')
+
+    img = Image.fromarray(pred_np, 'L')
+    img.save(os.path.join(home_dir,'Outputs/Alone/') + str(batchcount + 1) + '.png')
+
 
 
 # def val(dataloader, model, loss_fn, epochstep):
