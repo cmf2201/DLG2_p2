2e67d38344957873b3a7e7f4e4ca823759f770c3
Rumblemist <theradchad007@gmail.com>
2024-09-16 16:02:49 -0400

    i hope it works

--- test-video/test-video.py
+++ test-video/test-video.py
@@ -13,11 +13,9 @@ from torch.utils.data import Subset
 import wandb
 
 # CUSTOM
-from network import *
-from utils import *
+from part2.network import *
+from part2.utils import *
 from dataloader import *
-import pdb
-import utils
 
 # Load the parameters
 from loadParam import *
@@ -45,6 +43,56 @@ os.mkdir(JOB_FOLDER)
 os.mkdir(TRAINED_MDL_PATH)
 
 datatype = torch.float32
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cuda:1')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 dataset = VideoDataset(VIDEO_PATH)
+
+model = UNET().to("cpu")
+model.load_state_dict(torch.load("trained_model8.pth", map_location=torch.device('cpu')))
+
+model.eval()
+for batchcount, (rgb) in enumerate(dataset):
+    rgb = rgb.to(device)
+    
+    pred = model(rgb)
+    img = Image.fromarray(pred, 'L')
+    img.save(str(batchcount + 1) + '.png')
+
+
+# def val(dataloader, model, loss_fn, epochstep):
+#     model.eval()
+    
+#     epochloss = 0
+#     with torch.no_grad():
+#         for batchcount, (rgb, label) in enumerate(dataloader):
+#             dp(' batch', batchcount)
+            
+#             rgb = rgb.to(device)
+#             label = label.to(device)
+            
+#             pred = model(rgb)
+#             # print("Shapes: ")
+#             # print(pred.shape)   
+#             # print(label.shape)  
+#             loss = loss_fn(pred, label)  
+
+#             epochloss += loss.item()
+        
+#             wandb.log({
+#                 "batch/loss/": loss.item(),
+#                     })
+            
+#             if batchcount == 0: # only for the first batch every epoch
+#                 wandb_images = []
+#                 for (pred_single, label_single, rgb_single) in zip(pred, label, rgb):
+#                     combined_image_np = CombineImages(pred_single, label_single, rgb_single)
+#                     wandb_images.append(wandb.Image(combined_image_np))
+
+#                 wandb.log(
+#                 {
+#                     "images/val": wandb_images,
+#                 })
+            
+#     wandb.log({
+#         "epoch/loss/val": epochloss,
+#                 })
